55e80d00694c21b16dfdeef5a11f1142a2a6d15b
Mohammad Haneef <143437327+mohammadhaneef110@users.noreply.github.com>
2024-03-29 00:20:58 +0500

    Finish BMI Calculator

---
+++ main.js
@@ -0,0 +1,5 @@
+let weightInKg = 74;
+let heightInMeters = 1.85;
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`Your BMI is ${bmi}`);
+export {};
---
+++ main.ts
@@ -0,0 +1,4 @@
+let weightInKg = 74;
+let heightInMeters = 1.85;
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`Your BMI is ${bmi}`);
